f5e077381c9012f66de888927ae9147e0cdd3fae
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2023-07-26 18:15:08 -0400

    Actualización de la vista - Botón para agregar una nueva tarea con un edittext para escribir el nombre - Asigna fecha de creación y finalización en forma estática - Recursos extraídos para strings y dimens - Dentro de la entidad se mueve el id fuera del constructor primario para que no sea obligatorio entregarlo y pueda generarlo ROOM en la inserción

--- app/src/main/java/com/example/roomexample/MainActivity.kt
+++ app/src/main/java/com/example/roomexample/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.roomexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.roomexample.data.local.Task
+import com.example.roomexample.data.local.TaskDAO
+import com.example.roomexample.data.local.TaskDatabase
+import com.example.roomexample.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        loadData()
+
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.addTaskButton.setOnClickListener {
+            val taskName = binding.taskEditText.text.toString()
+            val task = Task(taskName, "26-07-2023", "27-07-2023")
+            GlobalScope.launch {
+                getDao().insertTask(task)
+            }
+        }
+    }
+
+    private fun loadData() {
+        GlobalScope.launch {
+            val tasks = getDao().getAllTask()
+
+            val tasksAsText = tasks.joinToString("\n") { it.task }
+            showTasks(tasksAsText)
+        }
+    }
+
+    private suspend fun showTasks(tasks: String) {
+        withContext(Dispatchers.Main) {
+            binding.showTasksTextView.text = tasks
+        }
+    }
+
+    private fun getDao(): TaskDAO {
+        return TaskDatabase.getDatabase(this@MainActivity).getTaskDao()
     }
 }
--- app/src/main/java/com/example/roomexample/data/local/Task.kt
+++ app/src/main/java/com/example/roomexample/data/local/Task.kt
@@ -6,10 +6,13 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 // Le indicamos a ROOM que esta data class va a ser una tabla en la BD
-@Entity(tableName="task_table")
+@Entity(tableName = "task_table")
 data class Task(
-    @PrimaryKey(autoGenerate = true) @NonNull val id: Long,
     val task: String,
     @ColumnInfo(name = "creation_date") val creationDate: String,
     @ColumnInfo(name = "finish_date") val finishDate: String
-)
+) {
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    var id: Long = 0
+}
